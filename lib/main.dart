2906a480c7cb75191922ed0d50c42e8eb5580244
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-23 07:23:19 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:advantage/constants/app_color.dart';
+import 'package:advantage/firebase_options.dart';
 import 'package:advantage/routes/app_page.dart';
 import 'package:advantage/screens/home/home_page.dart';
 import 'package:advantage/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -22,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppColor.primaryColor),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const SplashScreen(),
       getPages: AppPage.routes,
     );
   }
