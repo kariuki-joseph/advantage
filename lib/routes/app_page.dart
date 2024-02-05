f5a218c3b735cc8874a23e7d65ddf91de803b782
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-02-05 06:14:03 +0300

    Add post and update ad routes

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -4,10 +4,12 @@ import 'package:advantage/screens/phone_verification/phone_verification.dart';
 import 'package:advantage/screens/pin_login/confirm_pin.dart';
 import 'package:advantage/screens/pin_login/setup_pin.dart';
 import 'package:advantage/screens/pin_login/pin_login.dart';
+import 'package:advantage/screens/post_ad/post_ad.dart';
 import 'package:advantage/screens/register.dart';
 import 'package:advantage/screens/splash_screen.dart';
+import 'package:advantage/screens/update_ad/update_ad.dart';
 import 'package:advantage/screens/verification_success/verification_success.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class AppPage {
   static const String splash = "/splash";
@@ -19,6 +21,8 @@ class AppPage {
   static const String pinLogin = "/pin-login";
   static const String finishProfile = "/finish-profile";
   static const String home = "/home";
+  static const String postAd = "/post-ad";
+  static const String updateAd = "/update-ad";
 
   static List<GetPage> routes = [
     GetPage(name: splash, page: () => const SplashScreen()),
@@ -30,5 +34,17 @@ class AppPage {
     GetPage(name: pinLogin, page: () => const PinLogin()),
     GetPage(name: finishProfile, page: () => const FinishProfile()),
     GetPage(name: home, page: () => const HomePage()),
+    GetPage(
+      name: postAd,
+      page: () => PostAd(),
+      transition: Transition.native,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: updateAd,
+      page: () => UpdateAd(),
+      transition: Transition.zoom,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
   ];
 }
