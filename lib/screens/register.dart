07158b566c14c9d0ccd6a8e71240017477d8d143
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-04-07 09:04:38 +0300

    fix register screen to use theme colors

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,6 +1,8 @@
 import 'package:advantage/components/primary_button.dart';
+import 'package:advantage/routes/app_page.dart';
 import 'package:advantage/screens/auth/controllers/auth_controller.dart';
 import 'package:advantage/widgets/my_btn_loader.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -20,7 +22,6 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Form(
@@ -36,64 +37,9 @@ class _RegisterState extends State<Register> {
                 fit: BoxFit.contain,
               ),
               const SizedBox(height: 20),
-              const Text(
+              Text(
                 "Create An Account With Us",
-                style: TextStyle(fontSize: 28),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.only(left: 10.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Row(
-                  children: [
-                    DropdownButton<String>(
-                      underline: const SizedBox(),
-                      value: drowpdownValue,
-                      items: _countryCodes
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          drowpdownValue = value!;
-                        });
-                      },
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: "758826552",
-                          border:
-                              OutlineInputBorder(borderSide: BorderSide.none),
-                        ),
-                        keyboardType: TextInputType.phone,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Phone number is required";
-                          }
-                          if (value.length < 9) {
-                            return "Phone number is too short";
-                          }
-                          if (value.length > 9) {
-                            return "Phone number is too long";
-                          }
-
-                          return null;
-                        },
-                        onChanged: (value) {
-                          authController.phone.value = "$drowpdownValue$value";
-                        },
-                        controller: authController.phoneController,
-                      ),
-                    ),
-                  ],
-                ),
+                style: Get.theme.textTheme.headlineMedium,
               ),
               const SizedBox(height: 10),
               TextFormField(
@@ -137,6 +83,66 @@ class _RegisterState extends State<Register> {
                   return null;
                 },
               ),
+              const SizedBox(height: 10),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Get.theme.colorScheme.onPrimaryContainer,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    dense: true,
+                    contentPadding: EdgeInsets.zero,
+                    leading: DropdownButton<String>(
+                      underline: const SizedBox(),
+                      value: drowpdownValue,
+                      items: _countryCodes
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          drowpdownValue = value!;
+                        });
+                      },
+                    ),
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "758826552",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      keyboardType: TextInputType.phone,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Phone number is required";
+                        }
+                        if (value.length < 9) {
+                          return "Phone number is too short";
+                        }
+                        if (value.length > 9) {
+                          return "Phone number is too long";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        authController.phone.value = "$drowpdownValue$value";
+                      },
+                      controller: authController.phoneController,
+                    ),
+                  ),
+                ),
+              ),
               const SizedBox(height: 50),
               Obx(
                 () => PrimaryButton(
@@ -147,15 +153,31 @@ class _RegisterState extends State<Register> {
                   child: authController.isLoading
                       ? const MyBtnLoader(
                           large: true,
-                          color: Colors.white,
                         )
-                      : const Text(
+                      : Text(
                           "Get Started",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          style: Get.theme.textTheme.labelLarge,
                         ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: "Already have an account? ",
+                    style: Get.theme.textTheme.bodyLarge,
+                    children: [
+                      TextSpan(
+                        text: "Login",
+                        style: Get.theme.textTheme.bodyLarge?.copyWith(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => Get.toNamed(AppPage.pinLogin),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
