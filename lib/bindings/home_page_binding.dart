1d294790782661d31035cb63d5264e09235e6089
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-04-08 06:55:58 +0300

    add homebinding

---
+++ lib/bindings/home_page_binding.dart
@@ -0,0 +1,9 @@
+import 'package:advantage/screens/home/controller/home_page_controller.dart';
+import 'package:get/get.dart';
+
+class HomePageBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomePageController>(() => HomePageController());
+  }
+}
