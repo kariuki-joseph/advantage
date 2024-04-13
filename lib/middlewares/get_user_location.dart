d340952d0c2912eca77919b3f10c910f6b9ab3f3
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-04-13 15:59:47 +0300

    remove location middleware (not used)

--- lib/middlewares/get_user_location.dart
+++ lib/middlewares/get_user_location.dart
@@ -1,24 +1,3 @@
-import 'package:advantage/screens/home/controller/location_controller.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class GetUserLocation extends GetMiddleware {
-  final LocationController locationController =
-      Get.find(); // Get the instance of LocationController
-
-  @override
-  RouteSettings? redirect(String? route) {
-    debugPrint("redirecting to location configuration");
-    return super.redirect(route);
-  }
-
-  @override
-  Future<GetNavConfig?> redirectDelegate(GetNavConfig route) async {
-    debugPrint("Initializing location configuration");
-
-    // Perform an asynchronous task
-    await locationController.initConfig();
-
-    return super.redirectDelegate(route);
-  }
-}
+class GetUserLocation extends GetMiddleware {}
